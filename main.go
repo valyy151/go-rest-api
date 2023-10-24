53b9b7a8ccb44f32e1ea761c8e63ccf607410315
valyy151 <marin.valenta1@gmail.com>
2023-10-24 11:26:12 +0200

    get request

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type todo struct {
+	ID        string `json:"id"`
+	Item      string `json:"item"`
+	Completed bool   `json:"completed"`
+}
+
+var todos = []todo{
+	{ID: "1", Item: "Learn Go", Completed: false},
+	{ID: "2", Item: "Learn React", Completed: true},
+	{ID: "3", Item: "Learn German", Completed: false},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, todos)
+}
+
 func main() {
-	println("Hello, World!")
+	router := gin.Default()
+
+	router.GET("/todos", getTodos)
+	router.Run("localhost:8080")
+
 }
